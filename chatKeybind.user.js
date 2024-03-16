9bf03a37e5059b47a5a22e390ca42f83f7e3d436
lune <79767777+diary001@users.noreply.github.com>
2024-03-16 19:31:37 -0400

    Update chatKeybind.user.js to fix bugs

--- chatKeybind.user.js
+++ chatKeybind.user.js
@@ -3,14 +3,14 @@
 // @namespace    https://github.com/diary001/moonbounce
 // @supportURL   https://discord.gg/mbe
 // @license      MIT
-// @version      1.0.2
+// @version      1.0.3
 // @description  ((made with <3 by lune))
 // @author       lune / linktr.ee/moon
 // @match        *://*/*
 // @icon         https://framerusercontent.com/images/iuVZmsE2sifPExZXNrETQrDkU.png
 // @grant        none
-// @downloadURL  https://update.greasyfork.org/scripts/490052/Moonbounce%20Chat%20Keybind.user.js
-// @updateURL    https://update.greasyfork.org/scripts/490052/Moonbounce%20Chat%20Keybind.meta.js
+// @downloadURL https://update.greasyfork.org/scripts/490052/Moonbounce%20Chat%20Keybind.user.js
+// @updateURL https://update.greasyfork.org/scripts/490052/Moonbounce%20Chat%20Keybind.meta.js
 // ==/UserScript==
 
 (function() {
@@ -25,7 +25,9 @@
     function toggleChatbox() {
         const chatBoxContainer = document.querySelector("div#moonbounce-ext-container-mbheeaapbjpckahhciogfdodofjjldem");
         if (chatBoxContainer) {
-            const chatBoxButton = chatBoxContainer.shadowRoot.querySelector("#MOONBOUNCE\\.PORTAL > div:nth-child(2) > div > div > div > div._frame_1htaw_72 > button");
+            const chatBoxButton1 = chatBoxContainer.shadowRoot.querySelector("#MOONBOUNCE\\.PORTAL > div:nth-child(2) > div > div > div > div._frame_1htaw_72 > button");
+            const chatBoxButton2 = chatBoxContainer.shadowRoot.querySelector("#MOONBOUNCE\\.PORTAL > div:nth-child(4) > div > div > div > div._frame_1htaw_72 > button");
+            const chatBoxButton = chatBoxButton1 || chatBoxButton2;
             if (chatBoxButton) {
                 chatBoxButton.dispatchEvent(simulateClick);
             }
@@ -35,7 +37,9 @@
     function focusInput() {
         const inputContainer = document.querySelector("div#moonbounce-ext-container-mbheeaapbjpckahhciogfdodofjjldem");
         if (inputContainer) {
-            const input = inputContainer.shadowRoot.querySelector("#MOONBOUNCE\\.PORTAL > div:nth-child(2) > div > div > div._base_1jfm3_1._overlay_card_mdpvo_9 > div > div:nth-child(5) > div > form > div > input");
+            const input1 = inputContainer.shadowRoot.querySelector("#MOONBOUNCE\\.PORTAL > div:nth-child(2) > div > div > div._base_1jfm3_1._overlay_card_mdpvo_9 > div > div:nth-child(5) > div > form > div > input");
+            const input2 = inputContainer.shadowRoot.querySelector("#MOONBOUNCE\\.PORTAL > div:nth-child(4) > div > div > div._base_1jfm3_1._overlay_card_mdpvo_9 > div > div:nth-child(5) > div > form > div > input");
+            const input = input1 || input2;
             if (input) {
                 input.focus();
             }
