2ab63699c549bd4b6d06cc95bbb3b38585d1ced7
lune <138798718+Luna@users.noreply.github.com>
2024-05-15 18:20:16 -0400

    Update movementKeybinds.user.js

--- movementKeybinds.user.js
+++ movementKeybinds.user.js
@@ -3,14 +3,14 @@
 // @namespace    https://github.com/diary001/moonbounce
 // @supportURL   https://discord.gg/mbe
 // @license      MIT
-// @version      1.0.0
+// @version      1.0.1
 // @description  ((made with <3 by lune))
 // @author       lune / linktr.ee/moon
 // @match        *://*/*
 // @icon         https://framerusercontent.com/images/iuVZmsE2sifPExZXNrETQrDkU.png
 // @grant        none
-// @downloadURL  https://github.com/diary001/moonbounce/raw/main/movementKeybinds.user.js
-// @updateURL    https://github.com/diary001/moonbounce/raw/main/movementKeybinds.user.js
+// @downloadURL https://update.greasyfork.org/scripts/495082/Moonbounce%20Movement%20Keybinds.user.js
+// @updateURL https://update.greasyfork.org/scripts/495082/Moonbounce%20Movement%20Keybinds.meta.js
 // ==/UserScript==
 
 (function() {
