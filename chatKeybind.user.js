40e7f6c9a1e02b8825ec13c5a14d9efbaf92152d
lune <138798718+Luna@users.noreply.github.com>
2024-05-22 14:57:54 -0400

    Update chatKeybind.user.js

--- chatKeybind.user.js
+++ chatKeybind.user.js
@@ -4,7 +4,7 @@
 // @supportURL   https://discord.gg/mbe
 // @license      MIT
 // @version      1.0.4
-// @description  ((made with <3 by lune))
+// @description  Allows you to use the "/" key to open and close the chat. ((made with <3 by lune))
 // @author       lune / linktr.ee/moon
 // @match        *://*/*
 // @icon         https://framerusercontent.com/images/iuVZmsE2sifPExZXNrETQrDkU.png
